78525c282d30062a0110421cd9c81441e13cc807
Stanislaw Tkachenko <stas1696@mail.ru>
2023-12-07 21:19:31 +0400

    add commit Task_06.java

---
+++ src/Task_06.java
@@ -0,0 +1,17 @@
+public class Task_06 {
+    public static void main(String[] args) {
+        System.out.println("Задание №6");
+        int totalNumbers = 100000;
+        int excludedCount = 0;
+
+        for (int i = 0; i < totalNumbers; i++) {
+            String number = String.format("%05d", i);
+
+            if (number.contains("4") || number.contains("13")) {
+                excludedCount++;
+            }
+        }
+
+        System.out.println("Количество номеров, которые нужно исключить: " + excludedCount);
+    }
+}
